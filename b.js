63914704aa8c3ff70ea106176aee2b78417f9e1c
TS <xyneos@gmail.com>
2020-04-26 23:47:27 +0200

    second commit

---
+++ b.js
@@ -0,0 +1,57 @@
+console.log("aaaaa bbbbbb");
+console.log("3<11 " + (2<3));
+console.log("3"< "11");
+console.log("3" < 11);
+console.log("11" < 3);
+console.log();
+console.log("+0 == -0 ");
+console.log( +0 ==  -0 );
+console.log(" Object.is(+0, -0)");
+console.log(Object.is(+0, -0));
+console.log(" Object.is(0, -0)");
+console.log(Object.is(0, -0));
+console.log(" Object.is(+0, 0)");
+console.log(Object.is(+0, 0));
+console.log();
+console.log(" ");
+console.log();
+console.log(" ");
+console.log();
+
+console.log();
+console.log(" NaN == NaN ");
+console.log(NaN == NaN);
+console.log("NaN === NaN");
+console.log(NaN === NaN);
+console.log("Object.is(NaN, NaN)");
+console.log(Object.is(NaN, NaN));
+console.log(" ");
+console.log();
+console.log(" ");
+console.log();
+console.log(" ");
+console.log();
+console.log(" ");
+console.log(" ");
+console.log();
+console.log(" ");
+console.log();
+console.log(" ");
+console.log();
+console.log(" ");
+console.log(" ");
+console.log();
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log();
+console.log();
+console.log();
+console.log();
+console.log(" ");
+console.log(" ");
+
+
+
+
+
